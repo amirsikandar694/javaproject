66416d85b5ec8ba2222c01ada088f141e37c35d2
skf-project <74064182+skf-project@users.noreply.github.com>
2021-10-11 20:49:35 +0530

    Changed Input parameter

--- src/stringProgram/PalindromeString.java
+++ src/stringProgram/PalindromeString.java
@@ -5,7 +5,7 @@ public class PalindromeString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s1="nitin",str="";
+		String s1="Amir",str="";
 		int i;
 		int stringlenghth=s1.length()-1;
 		for(i=stringlenghth;i>=0;i--)
